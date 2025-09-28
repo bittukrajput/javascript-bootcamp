66772f3dba84259216fd6184a874c00de427c5d3
bittukrajput <bittu2537476@gmail.com>
2025-09-28 14:53:40 +0530

    Ternary Operator in JavaScript

---
+++ 12_TERNARY_OPERATOR/script.js
@@ -0,0 +1,10 @@
+// debugger
+// const userName='Anurag'
+// const gender='F'
+
+// const userMessage=`${gender.toLowerCase==='F'?'She':'He'} is a student`
+// console.log(userName)
+
+const result=null?'Anurag':'Singh'?10:0
+console.log(result)
+

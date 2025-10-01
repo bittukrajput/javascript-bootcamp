682c0b3641b62b90568eb04e8b88b44cd036ebfa
bittukrajput <bittu2537476@gmail.com>
2025-10-02 04:28:59 +0530

    Compound Operator and Unary Operator in JavaScript

---
+++ 20_COMPOUND_OPERATOR/script.js
@@ -0,0 +1,50 @@
+let num=2;
+
+num+=2 // num=num+2
+//debugger
+
+
+//num=num+2
+// num=num+2
+// num=num+2
+// num=num+2
+// num=num+2
+// num=num+2
+// num=num+2
+// num=num+2
+//Addition (+=)
+
+let x = 10;
+x += 5; // equivalent to x = x + 5
+console.log(x); // 15
+
+//subtraction(-=)
+
+let y=20;
+y-=5; // equivalent to y=y-5
+console.log(y)
+
+// Multiplication(*=)
+
+let z= 4;
+z *= 3; // equivalent to y = y * 3
+console.log(z); // 12
+
+// modulus(%=)
+
+let a= 17;
+a %= 3; // equivalent to z = z % 3
+console.log(a); // 2
+
+// Increment 
+
+// post fix increment // use present value then increment
+let n=5;
+console.log(n++); //5
+console.log(n); //6
+
+// pre fix increment :-->> increment first then return the value
+
+let preFix=5
+console.log(++preFix); //6
+console.log(preFix); //6

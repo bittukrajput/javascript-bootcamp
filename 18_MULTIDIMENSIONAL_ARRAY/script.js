478e7200bce193ca1f043fd21b372f9d0d81ce48
bittukrajput <bittu2537476@gmail.com>
2025-10-02 01:44:46 +0530

    Multidimensional Array in javascript

---
+++ 18_MULTIDIMENSIONAL_ARRAY/script.js
@@ -0,0 +1,15 @@
+const nameAndNumber = [
+  ["Adarsh", 80],
+  ["Akash", 90],
+  ["Anurag", 9],
+];
+const ticTacToe=[
+    ['X',null,null],
+    [null,null,'O'],
+    ['O',null,'X']
+];
+const printNumber=[
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+];

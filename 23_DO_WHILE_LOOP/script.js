c837687b7b5e4855f9f9d8f3ccb7c5542b097598
bittukrajput <bittu2537476@gmail.com>
2025-10-02 06:56:52 +0530

    While Loop in javascript

---
+++ 23_DO_WHILE_LOOP/script.js
@@ -0,0 +1,17 @@
+console.log("Program Started");
+//debugger
+let i=0;
+do{
+    console.log(i);
+    i++;
+}while(i<5)
+
+const fruits = ["Mango", "Apple", "Banana"];
+let index = 0;
+do {
+    console.log(fruits[index]);
+    index++;
+} while(index < fruits.length);
+
+
+console.log("Program ended!");

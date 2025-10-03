a0de4de7368167e7e9d9b3e5a3021f885b550ae2
bittukrajput <bittu2537476@gmail.com>
2025-10-03 17:03:21 +0530

    Hoisting in Javascript

---
+++ 28_HOISTING/script.js
@@ -0,0 +1,17 @@
+
+console.log(userName);
+
+var userName="Anurag";
+
+Hi();
+sayHi();
+//function definition
+// function declaration
+function Hi(){
+    console.log("Hello World");
+}
+// function expression
+const sayHi=function(){ // anonymous function
+    console.log("Hiiiiiiiiiiii,")
+}
+//sayHi();

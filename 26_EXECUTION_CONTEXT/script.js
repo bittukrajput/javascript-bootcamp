84195f2239d176913c37ac96163beb1f7f977f52
bittukrajput <bittu2537476@gmail.com>
2025-10-03 06:40:01 +0530

    Execution context in javascript

---
+++ 26_EXECUTION_CONTEXT/script.js
@@ -0,0 +1,17 @@
+debugger
+
+
+sayHii();
+
+const userName="Anurag";
+const userAge=28;
+
+function sayHii(a,b){
+    //console.log("Hii");
+    a=10;
+    b=15;
+    add(7,9);
+}
+function add(x,y){
+    return x+y;
+}

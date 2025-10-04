9bf2796e7c7ca939eee61d902eea60c1357a8f76
bittukrajput <bittu2537476@gmail.com>
2025-10-04 15:44:46 +0530

    Local and Global Scope in JavaScript

---
+++ 29_SCOPE_IN_JAVASCRIPT/script.js
@@ -0,0 +1,23 @@
+debugger
+
+const userName="Anurag";
+const userAge=28;
+var a=50;
+function add(){
+    const x=10;
+    const y=20;
+    console.log(x+y);
+    console.log(userName);  
+}
+
+function subtract(){
+    const x=10;
+    const y=20;
+    console.log(x-y);
+    console.log(userName);  
+}
+
+add();
+subtract();
+console.log("Program Ended!");
+
